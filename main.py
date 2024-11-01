02482ba2abb6024e9d636f5f8a7c20428cc73510
Vishu Vaishnav <76559999+vishuvaishnav@users.noreply.github.com>
2024-11-01 14:04:56 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from scrape import (
+    scrape_website,
+    extract_body_content,
+    clean_body_content,
+    split_dom_content,
+)
+from parse import parse_with_ollama
+
+st.title("Web Scraper with LLM")
+url = st.text_input("Enter the URL")
+
+if st.button("SCRAPE"):
+    if url:
+        dom_content = scrape_website(url)
+        body_content = extract_body_content(dom_content)
+        cleaned_content = clean_body_content(body_content)
+
+        st.session_state.dom_content = cleaned_content
+
+        with st.expander("View DOM Content"):
+            st.text_area("DOM Content", cleaned_content, height=300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse")
+
+    if st.button("PARSE"):
+        if parse_description:
+
+            # Parse the content with Ollama
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            parsed_result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(parsed_result)
